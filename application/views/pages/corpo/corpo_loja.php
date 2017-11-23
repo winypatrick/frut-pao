eac92e2f2e4460eae80e7a57f0d81e6356b12931
unknown <adidas.coyas@gmail.com>
2017-11-23 17:36:35 -0100

    files da paginas da loja

--- application/views/pages/corpo/corpo_loja.php
+++ application/views/pages/corpo/corpo_loja.php
@@ -1,10 +1,13 @@
 <style media="screen">
-  section {
-    margin-bottom: 2%;
-  }
+.afasta {
+  margin-left: 5%;
+}
+.longe {
+  margin-left: 10%;
+}
 </style>
 
-<script type="text/javascript" src="<?php base_url();?>/fich_jquery/loja.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>/fich_jquery/loja.js"></script>
 <section class="content-header">
   <ol class="breadcrumb">
     <li><a href="<?= base_url();?>funcionario"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -15,79 +18,143 @@
 
 <div class="container-fluid">
   <div class="row">
-    <div class="col-md-12">
-      <div class="box">
-        <div class="box-header with-border">
-         <!--  <h3 class="box-title">Funcionarios</h3> -->
+   <div class="col-md-12">
+   <div class="box">
+     <div class="box-header with-border ">
 
-         <!--  <a type="button"  class="fa fa-plus-circle btn" data-toggle="modal" data-target="#myModal"></a> -->
-          <a type="button" id="adicionar" class="fa fa-plus-circle btn btn-primary btn-xs" style="display: none;  "> Novo </a>
+       <a type="button" id="add" class="fa fa-plus-circle btn btn-info btn-xs" style="" onclick="modal_loja()">Adicionar Loja </a>
 
+       <div class="box-tools pull-right" >
 
-          <div class="box-tools pull-right">
-         <span type="button" class="btn btn-box-tool" ><i class="fa fa-minus"></i>
-
-         </span>
-          </div>
-        </div>
-
-        <!-- box-body -->
-        <div class="box-body">
-
-          <table class="table table-striped table-condensed " id="tabl">
-                              <thead id="thead1">
-                             <th>Nome  (Detalhes)</th>
-                             <th  style="width: 17%">Morada</th>
-                             <th>Função</th>
-                             <th>Email</th>
-                             <th>Contacto</th>
-                             <th >Acesso</th>
-                             <th id="edit_del" style="display: none;">Edit | Delete</th>
-
-                                 </thead>
-                                 <tbody >
-
-                                 </tbody>
-
-                        <tfoot>
-                        <tr>
-                          <th></th>
-                          <th></th>
-                          <th></th>
-                          <th></th>
-                          <th style="display: none;"></th>
-                          <th style="display: none;"></th>
-                          <th style="display: none;"></th>
-                        </tr>
-                        </tfoot>
-
-                            </table>
-
-
-        </div>
-       <!-- ./box-body -->
-
-
-        <!-- box-footer -->
-        <div class="box-footer">
-
-        </div>
-        <!-- /.box-footer -->
-
-      </div>
-      <!-- /.box -->
+    <div class=" inputWithIcon" >
+    <input  placeholder="search"  id="pesk_1" name="password" onkeyup=""  type="text" value="" style="width: 200px; height: 23px" required>
+    <i class="fa fa-search" ></i>
     </div>
+       </div>
+     </div>
+
+     <!-- box-body -->
+     <div id=" " class="box-body" style="">
+       <div class="row ">
+         <div class="col-md-6">
+           <div class="box">
+             <div class="box-header with-border">
+               <i class="fa fa-wpforms" aria-hidden="true"></i>
+               <b class="afasta"> <span id="lojaLocal">Fazenda</span> </b>
+               <b class="afasta"> <span id="lojaRua">rua abilio cabral</span> </b>
+               <b class="longe"> <span id="lojaContacto">2640987</span> </b>
+               <div class="box-tools pull-right">
+                 <button type="button" class="btn btn-default" onclick="insert_loja()">Detalhes</button>
+               </div>
+             </div>
+           </div>
+           <div class="box">
+             <div class="box-header with-border">
+               <i class="fa fa-wpforms" aria-hidden="true"></i>
+               <b class="afasta"> <span id="lojaLocal">Fazenda</span> </b>
+               <b class="afasta"> <span id="lojaRua">rua abilio cabral</span> </b>
+               <b class="longe"> <span id="lojaContacto">2640987</span> </b>
+               <div class="box-tools pull-right">
+                 <button type="button" class="btn btn-default">Detalhes</button>
+               </div>
+             </div>
+           </div>
+           <div class="box">
+             <div class="box-header with-border">
+               <i class="fa fa-wpforms" aria-hidden="true"></i>
+               <b class="afasta"> <span id="lojaLocal">Fazenda</span> </b>
+               <b class="afasta"> <span id="lojaRua">rua abilio cabral</span> </b>
+               <b class="longe"> <span id="lojaContacto">2640987</span> </b>
+               <div class="box-tools pull-right">
+                 <button type="button" class="btn btn-default">Detalhes</button>
+               </div>
+             </div>
+           </div>
+           <div class="box">
+             <div class="box-header with-border">
+               <i class="fa fa-wpforms" aria-hidden="true"></i>
+               <b class="afasta"> <span id="lojaLocal">Fazenda</span> </b>
+               <b class="afasta"> <span id="lojaRua">rua abilio cabral</span> </b>
+               <b class="longe"> <span id="lojaContacto">2640987</span> </b>
+               <div class="box-tools pull-right">
+                 <button type="button" class="btn btn-default">Detalhes</button>
+               </div>
+             </div>
+           </div>
+           <div class="box">
+             <div class="box-header with-border">
+               <i class="fa fa-wpforms" aria-hidden="true"></i>
+               <b class="afasta"> <span id="lojaLocal">Fazenda</span> </b>
+               <b class="afasta"> <span id="lojaRua">rua abilio cabral</span> </b>
+               <b class="longe"> <span id="lojaContacto">2640987</span> </b>
+               <div class="box-tools pull-right">
+                 <button type="button" class="btn btn-default">Detalhes</button>
+               </div>
+             </div>
+           </div>
+         </div>
+         <div class="col-md-6">
+           <div class="box">
+             <img src="im.png" width="500"  height="200" alt="">
+           </div>
+           <table class="table">
+             <thead>
+               <tr>
+                 <th>Ranking</th>
+                 <th>Zona</th>
+                 <th>Relatorios novos</th>
+                 <th>Caixa</th>
+                 <th>Ver</th>
+               </tr>
+             </thead>
+             <tbody>
+               <tr>
+                 <td>1º</td>
+                 <td>Fazenda</td>
+                 <td>2</td>
+                 <td>30.000</td>
+                 <td><button type="button" class="btn btn-default">Detalhes</button></td>
+               </tr>
+               <tr>
+                 <td>2º</td>
+                 <td>Achada S. Antonio</td>
+                 <td>2</td>
+                 <td>22.000</td>
+                 <td><button type="button" class="btn btn-default" >Detalhes</button></td>
+               </tr>
+               <tr>
+                 <td>3º</td>
+                 <td>São Domingos</td>
+                 <td>2</td>
+                 <td>18.000</td>
+                 <td><button type="button" class="btn btn-default">Detalhes</button></td>
+               </tr>
+             </tbody>
+           </table>
+         </div>
+       </div>
+     </div>
+    <!-- ./box-body -->
+
+
+     <!-- box-footer -->
+     <div class="box-footer">
+
+
+     <!-- /.box-footer -->
+
+   </div>
+   <!-- /.box -->
+ </div>
+ <!-- /.col -->
+</div>
+
+</div>
+<!-- /.row -->
   </div>
-  <div class="row ">
-    <div class="col-md-6">
 
-    </div>
-    <div class="col-md-6">
-
-    </div>
-  </div>
   <!-- Inicio do modal criar nova loja -->
-  <div class="modal fade" id="modal-1">
+  <div class="modal fade" id="modal-loja">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
